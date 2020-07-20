bee6ce508a09e069477ef2a2a3b9d4d4d18abfe9
Sujit Jaiwaliya <2017csb1115@iitrpr.ac.in>
2020-07-20 11:40:52 +0530

    Google SignIn Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:chatroom/screens/home_screen.dart';
+import 'package:chatroom/screens/login_screen.dart';
+import 'package:chatroom/services/firebase_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
 
 // TODO: open ios/Runner.xcworkspace to open xcode
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  FirebaseRepository _repository = FirebaseRepository();
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: FutureBuilder(
+        future: _repository.getCurrentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return LoginScreen();
+          }
+        },
+      ),
     );
   }
 }
